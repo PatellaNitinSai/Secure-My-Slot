222ea3993d6b37af5471ecdf35cba6a51bb53617
PatellaNitinSai <nitinsaipatella2024@gmail.com>
2025-11-19 16:44:46 +0530

    Update server.js

--- backend/server.js
+++ backend/server.js
@@ -14,7 +14,7 @@ connectDB();
 // app.use('/api', loginRoute);
 
 app.use(cors({
-  origin: 'http://localhost:4200',
+  origin: 'https://secure-my-slot.onrender.com',
   credentials: true,
   methods: ['GET','POST','PUT','PATCH','DELETE','OPTIONS'],
   allowedHeaders: ['Content-Type','Authorization']
