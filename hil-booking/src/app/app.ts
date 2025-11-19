c653be21cfd1b79af13f05055c2ed8f48b5f3427
Sai <patesai@tbdir.net>
2025-11-19 15:36:34 +0530

    root changes

--- hil-booking/src/app/app.ts
+++ hil-booking/src/app/app.ts
@@ -1,10 +1,11 @@
 // src/app/app.ts
 import { Component } from '@angular/core';
 import { RouterOutlet } from '@angular/router';
+import { LoginForm } from './components/login-form/login-form';
 @Component({
   selector: 'app-root',
   standalone: true,
-  imports: [RouterOutlet], // only what the template uses
+  imports: [RouterOutlet,LoginForm], // only what the template uses
   templateUrl: './app.html'
 })
 export class App {}
